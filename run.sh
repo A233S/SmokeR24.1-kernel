a75c9741d41b9aa83f56eb0c4d1abcbcd9a39a73
A233S <71011762+A233S@users.noreply.github.com>
2022-07-29 14:20:44 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,13 @@
+git clone https://android.googlesource.com/platform/prebuilt
+export PATH=$(pwd)/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin:$PATH
+export ARCH=arm
+
+export SUBARCH=arm
+
+export CROSS_COMPILE=arm-eabi-
+
+
+
+make goldfish_defconfig  
 make  angler_defconfig
 make
