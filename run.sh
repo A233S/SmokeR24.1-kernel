1912c3594c56956850af5a338134cd9ed3421f60
A233S <71011762+A233S@users.noreply.github.com>
2022-07-29 14:33:24 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
-git clone https://android.googlesource.com/platform/prebuilt
-export PATH=$(pwd)/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin:$PATH
-export ARCH=arm
+sudo add-apt-repository ppa:openjdk-r/ppa
+sudo apt-get update
+sudo apt-get install openjdk-8-jdk
+sudo apt-get install git-core gnupg flex bison gperf libsdl1.2-dev \
+  libesd0-dev libwxgtk2.8-dev squashfs-tools build-essential zip curl \
+  libncurses5-dev zlib1g-dev pngcrush schedtool libxml2 libxml2-utils \
+  xsltproc lzop libc6-dev schedtool g++-multilib lib32z1-dev lib32ncurses5-dev \
+  lib32readline-gplv2-dev gcc-multilib libswitch-perl
 
-export SUBARCH=arm
-
-export CROSS_COMPILE=arm-eabi-
-
-
-
-make goldfish_defconfig  
-make  angler_defconfig
-make
+sudo apt-get install gcc-arm-linux-gnueabihf \
+  libssl1.0.0 libssl-dev \
+  p7zip-full
+bash build.sh
